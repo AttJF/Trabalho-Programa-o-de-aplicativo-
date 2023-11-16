cf9f0b8f7d908dfee06d53c83c8f9fdba3bb79dc
Gabriel Stawny <gastawny@gmail.com>
2023-11-16 17:49:19 -0300

    Implementação das Entidades Medico e Paciente

--- src/dao/MedicoDAO.java
+++ src/dao/MedicoDAO.java
@@ -1,5 +1,9 @@
 package dao;
 
 public class MedicoDAO {
-
+	public void cadastrar() { }
+	public void editar() { }
+	public void deletar() { }
+	public void buscarPorNome() { }
+	public void buscarTodos() { }
 }
--- src/entities/Medico.java
+++ src/entities/Medico.java
@@ -1,5 +1,84 @@
 package entities;
 
 public class Medico {
+	private int ID, CRM;
+	private String telefone, nome;
+	private Endereco endereco;
+	private Especialidade especialidade;
 
+	public Medico(int ID, int CRM, String telefone, String nome, Endereco endereco, Especialidade especialidade) {
+		this.ID = ID;
+		this.CRM = CRM;
+		this.telefone = telefone;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.especialidade = especialidade;
+	}
+
+
+	public int getID() {
+		return ID;
+	}
+
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+
+	public int getCRM() {
+		return CRM;
+	}
+
+
+	public void setCRM(int cRM) {
+		CRM = cRM;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+
+
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Medico [ID=" + ID + ", CRM=" + CRM + ", telefone=" + telefone + ", nome=" + nome + ", especialidade="
+				+ especialidade + "]";
+	}
 }
