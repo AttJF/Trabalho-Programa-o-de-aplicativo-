afe0d7ac537fc610247a75c292d14a6def929a41
Gabriel Stawny <gastawny@gmail.com>
2023-12-06 21:26:45 -0300

    Classe Validacao

---
+++ src/utils/Validacao.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Validacao {
+	public void verificaPositivo(int num) throws Exception {
+		if(num <= 0) throw new Exception("Numero igual ou menor que zero");
+	}
+	
+	public void verificaNaoVazio(String str) throws Exception {
+		if(str.isEmpty()) throw new Exception("String vazia");
+	}
+}
